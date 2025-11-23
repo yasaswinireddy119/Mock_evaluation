a8f508dc6284427fbf322782670db9c9a11e20ed
yasaswinireddy119 <yasaswinireddykotha@gmail.com>
2025-11-23 19:26:49 +0530

    mockevoluation_partb

---
+++ part-B/q8.js
@@ -0,0 +1,38 @@
+//2.1 Inbuilt HOF $ CBF's
+//question-8
+let num =[10 , 3 , 7 ,20 , 13 , 2]
+const square = Map.num(() => num * num)
+console.log(square)
+//filter
+const filter_ans=Map.filter.(
+    if num%2==0{
+        filter_ans.push()
+    }
+)
+console.log(filter_ans)
+
+
+//reduce
+
+const reduce_ans=reduce.num({total , n => total+n , 0});
+console.log(reduce_ans)
+
+//sort
+num_sort= num.sort(a-b)
+console.log(num_sort)
+
+
+
+///question - 9
+function displayCar()
+function displayTruck()
+function displayBike()
+
+vehicleInfo(vehicleCategory, callbackFn){
+    vehicleInfo("Car", displayCar)
+    vehicleInfo("Truck", displayTruck)
+    vehicleInfo("Bike", displayBike)
+
+}
+
+
